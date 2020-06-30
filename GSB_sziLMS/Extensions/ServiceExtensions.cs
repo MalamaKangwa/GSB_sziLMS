a29748eaf1cc0babd4b187fd87d9991a7b9f9911
MalamaKangwa <MalamaKangwa@szi.gov.zm>
2020-06-30 12:23:32 +0200

    Task: Addign Logger Service

---
+++ GSB_sziLMS/Extensions/ServiceExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Contracts;
+using LoggerService;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GSB_sziLMS.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureLoggerService(this IServiceCollection services) =>
+            services.AddScoped<ILoggerManager, LoggerManager>();
+    }
+}
--- GSB_sziLMS/Startup.cs
+++ GSB_sziLMS/Startup.cs
@@ -10,6 +10,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using GSB_sziLMS.Extensions;
+using NLog;
+using System.IO;
 
 namespace GSB_sziLMS
 {
@@ -17,6 +20,8 @@ namespace GSB_sziLMS
     {
         public Startup(IConfiguration configuration)
         {
+            LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+
             Configuration = configuration;
         }
 
@@ -25,6 +30,7 @@ namespace GSB_sziLMS
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.ConfigureLoggerService();
             services.AddControllers();
         }
 
